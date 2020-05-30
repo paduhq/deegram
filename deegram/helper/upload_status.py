1986517cda036ae64973e197bee36616fcf2cfc9
Aykut Yilmaz <aykuxt@gmail.com>
2020-05-30 18:07:22 +0200

    Make attributes of UploadStatus private

--- deegram/helper/upload_status.py
+++ deegram/helper/upload_status.py
@@ -10,36 +10,36 @@ logger = logging.getLogger(__name__)
 
 class UploadStatus:
     def __init__(self, event, track_count: int = None, total_tracks: int = None):
-        self.current = 0
-        self.total = 0
-        self.event = event
+        self._current = 0
+        self._total = 0
+        self._event = event
+        self._message = None
         self._start_time = 0.0
         self._update_time = 0.0
-        self.message = None
-        self.track_count = track_count
-        self.total_tracks = total_tracks
+        self._track_count = track_count
+        self._total_tracks = total_tracks
 
     @property
     def upload_speed(self) -> float:
-        return self.current / (time.time() - self._start_time)
+        return self._current / (time.time() - self._start_time)
 
     async def start(self) -> None:
         self._start_time = time.time()
-        self.message = await self.event.reply("Uploading...")
+        self._message = await self._event.reply("Uploading...")
 
     async def progress(self, current: int, total: int) -> None:
-        self.current = current
-        self.total = total
+        self._current = current
+        self._total = total
         if (time.time() - self._update_time) > 1:
             msg = ""
-            if self.track_count:
-                msg += f" 💿 Track {self.track_count} of {self.total_tracks}\n"
+            if self._track_count:
+                msg += f" 💿 Track {self._track_count} of {self._total_tracks}\n"
             msg += (
-                f"🔼 Uploading... {(self.current / self.total):.1%}\n"
+                f"🔼 Uploading... {(self._current / self._total):.1%}\n"
                 f"⚡ Speed: {get_readable_file_size(self.upload_speed)}/s"
             )
             try:
-                await self.message.edit(msg)
+                await self._message.edit(msg)
             except MessageNotModifiedError:
                 logger.debug("Message not modified")
             except ZeroDivisionError:
@@ -48,4 +48,4 @@ class UploadStatus:
                 self._update_time = time.time()
 
     async def finished(self) -> None:
-        await self.message.delete()
+        await self._message.delete()
